b67cb00884652c6eb917c819f55f72982ebae3fc
alparslanakbas <bybluestht@gmail.com>
2025-02-09 22:33:45 +0300

    Added
    - Necessary operations were performed on a sample class in the Infrastructure layer.
    - Added postgre.
    - Added DI.
    - Added Scrutor package for DI.

---
+++ src/BaseCleanArchitecture.Infrastructure/InfrastructureRegister.cs
@@ -0,0 +1,36 @@
+﻿using BaseCleanArchitecture.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseCleanArchitecture.Infrastructure
+{
+    public static class InfrastructureRegister
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                string connectionString = configuration.GetConnectionString("DefaultConnection")!;
+                options.UseNpgsql(connectionString);
+            });
+
+            services.Scan(options =>
+            {
+                options.FromAssemblies(typeof(InfrastructureRegister).Assembly)
+                .AddClasses(publicOnly: false)
+                .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+                .AsImplementedInterfaces()
+                .WithScopedLifetime();
+            });
+
+            return services;
+        }
+    }
+}
---
+++ src/BaseCleanArchitecture.Infrastructure/Repositories/EmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using BaseCleanArchitecture.Domain.Employees;
+using BaseCleanArchitecture.Infrastructure.Context;
+using GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseCleanArchitecture.Infrastructure.Repositories
+{
+    internal sealed class EmployeeRepository : Repository<Employee, ApplicationDbContext>, IEmployeeRepository
+    {
+        public EmployeeRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
